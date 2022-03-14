603341d6c5a7264a0061a5f0f109fa652b7570de
maria1443 <101413690+maria1443@users.noreply.github.com>
2022-03-14 10:09:54 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,103 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StyleSheet, ScrollView, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import NuevaOrden from './views/NuevaOrden';
+import Menu from './views/Menu';
+import DetallePlatillo from './views/DetallePlatillo';
+import FormularioPlatillo from './views/FormularioPlatillo';
+import ResumenPedido from './views/ResumenPedido';
+import ProgresoPedido from './views/ProgresoPedido';
+
+//Components
+import BotonResumen from './components/ui/BotonResumen';
+
+
+// importar state de context
+import FirebaseState from './context/firebase/firebaseState';
+import PedidoState from './context/pedidos/pedidosState';
+import {decode, encode} from 'base-64';if (!global.btoa) { global.btoa = encode }if (!global.atob) { global.atob = decode }
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+  return (
+    <>
+    <FirebaseState>
+      <PedidoState>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#FFDA00'
+              },
+              headerTitleStyle:{
+                fontWeight: 'bold'
+              }
+            }}  
+          >
+
+          <Stack.Screen
+            name='NuevaOrden'
+            component={NuevaOrden}
+            options={{
+              title: "Nueva Orden"
+            }}
+          />
+
+          <Stack.Screen
+            name='Menu'
+            component={Menu}
+            options={{
+              title: "Nuestro Menú",
+              headerRight: props => <BotonResumen/>
+            }}
+          />
+
+          <Stack.Screen
+            name='DetallePlatillo'
+            component={DetallePlatillo}
+            options={{
+              title: "Detalle del Platillo"
+            }}
+          />
+
+          <Stack.Screen
+            name='FormularioPlatillo'
+            component={FormularioPlatillo}
+            options={{
+              title: "Ordenar Platillo"
+            }}
+          />
+
+          <Stack.Screen
+            name='ResumenPedido'
+            component={ResumenPedido}
+            options={{
+              title: "Resumen del Pedido"
+            }}
+          />
+
+          <Stack.Screen
+            name='ProgresoPedido'
+            component={ProgresoPedido}
+            options={{
+              title: "Progreso del Pedido"
+            }}
+          />
+
+
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PedidoState>
+    </FirebaseState>
+    </>
+  );
+};
+
+
+export default App;
+
